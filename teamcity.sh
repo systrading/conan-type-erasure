c5ead04efc75eb9a395be106e5a697b00ef87ded
Antoine Waugh <antoine@relativetechnologies.com>
2023-10-17 16:22:50 +1100

    Update to use symbio2-devtools

--- teamcity.sh
+++ teamcity.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-image=092165354856.dkr.ecr.ap-southeast-2.amazonaws.com/symbio-devtools:latest
 
-eval $(aws ecr get-login --region ap-southeast-2 | sed -e 's/-e none//g')
+image=821543356141.dkr.ecr.ap-southeast-2.amazonaws.com/symbio2-devtools:latest
+
+eval $(aws ecr get-login --profile=prod --region ap-southeast-2 | sed -e 's/-e none//g')
 
 docker pull ${image}
 
